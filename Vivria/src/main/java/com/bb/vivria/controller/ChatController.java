8381a613a5906da2e5e11bae7904969721ffe76a
thkmon <thkmon@gmail.com>
2021-07-02 03:53:31 +0900

    채팅 페이지 추가

--- Vivria/src/main/java/com/bb/vivria/controller/ChatController.java
+++ Vivria/src/main/java/com/bb/vivria/controller/ChatController.java
@@ -1,8 +1,16 @@
 package com.bb.vivria.controller;
 
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ChatController {
-	
+	@RequestMapping(value = "/chat", method = RequestMethod.GET)
+	public String chat(Locale locale, Model model) {
+		return "chat/index.jsp";
+	}
 }
